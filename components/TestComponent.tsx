3e43c329bec2cc31e525e0eaa965ec459476f96d
Kelvin Ugochukwu Arinze <iamkelz911@gmail.com>
2026-07-01 06:39:38 +0100

    ACE Global Group v1.0 Launch

--- components/TestComponent.tsx
+++
@@ -1,3 +0,0 @@
-export default function TestComponent() {
-  return <h2>Hello from Test Component</h2>;
-}
--- components/TestHero.tsx
+++
@@ -1,9 +0,0 @@
-export default function TestHero() {
-  return (
-    <section className="pt-40 p-10">
-      <h1 className="text-5xl font-bold">
-        Logistics Test
-      </h1>
-    </section>
-  );
-}
